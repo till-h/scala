7610b92c178d0df036503d23ff72fab10955105d
Till Hackler <till.hackler@bbc.co.uk>
2014-12-05 18:00:49 +0000

    Brackets and multiplication only seem to work, other operators don't.

--- arithmeticparser.scala
+++ arithmeticparser.scala
@@ -1,12 +1,18 @@
 object parser {
 	// constructor
-	var iteration = 0
-	val operators = List("*","/","+","-")
-	val re_innermost_bracket = """(\(([^\)^\(]+)\))""".r
+	private var iteration = 0
+	private val operators = List("*","/","+","-")
+	private val re_innermost_bracket = """(\([^\)\(]+\))""".r
 
-	def apply(str: String) { eval(str) }
+	private def split_and_eval(str: String, re: String) = {
+		var left = str.split(re, 2)(0)
+		var right = str.split(re, 2)(1)
+		if (operators.exists(left.contains(_))) { left = eval(left) }
+		if (operators.exists(right.contains(_))) { right = eval(right) }
+		(left, right)
+	}
 
-	def eval(str: String): String = {
+	private def eval(str: String): String = {
 		iteration = iteration + 1
 		println(iteration.toString ++ "\t" ++ str)
 		str match {
@@ -18,25 +24,42 @@ object parser {
 			}
 			// should only get here if no brackets are left inside current substring
 			case str if str.contains("*") => {
-				var left = str.split("\\*")(0)
-				var right = str.split("\\*")(1)
-				if (operators.exists(left.contains(_))) { left = eval(left)}
-				if (operators.exists(right.contains(_))) { right = eval(right)}
+				val (left, right) = split_and_eval(str, "\\*")
 				val res = left.toFloat * right.toFloat
-				eval(res.toString)
+				res.toString
 			}
-			case str if str.contains("/") => { str }
+			case str if str.contains("/") => {
+				val (left, right) = split_and_eval(str, "/")
+				val res = left.toFloat / right.toFloat
+				res.toString
+			}
 			// should only get here if no priority operations * or / are left inside current substring
-			case str if str.contains("+") => { str }
-			case str if str.contains("-") => { str }
+			case str if str.contains("+") => {
+				val (left, right) = split_and_eval(str, "\\+")
+				val res = left.toFloat + right.toFloat
+				res.toString
+			}
+
+			case str if str.contains("-") => {
+				val (left, right) = split_and_eval(str, "-")
+				val res = left.toFloat - right.toFloat
+				res.toString
+			}
 			case str_no_operators_left => { str_no_operators_left }
 		}
 	}
+
+	def apply(str: String) = eval(str)
 }
 
 object Main extends App {
-	//println(parser("5*(3*2)+7*(9-5)-((7/6)*2)"))
-	println(parser("5*(3*2)"))
+	//println(parser("5*(3*2)"))
+	//println(parser("2*(3*4)*(5*(6*7))"))
+	//println(parser("2*(3*4)*(5*(6*7))*(2*6*7*1*(6*7*(8*(9)*4*2)*1)*1)*7"))
+	println(parser("1+2*3"))
+	//println(parser("1+10*2/2"))
+	//println(parser(""))
+	//println(parser(""))
 	//println(res = parser("1-1"))
 	//println(res = parser("1*1"))
 	//println(res = parser("1/1"))
